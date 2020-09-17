1d0f624fb7b9b9f5a8983b5f7ca4d761a6c7d869
Jassak <jassak@me.com>
2020-09-17 10:59:22 +0200

    Initial Commit

---
+++ RPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RPS
+//
+//  Created by Kardan on 17/09/2020.
+//  Copyright © 2020 Kardan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
